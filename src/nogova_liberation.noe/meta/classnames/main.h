7ad22caabce32c56fb98ea22e260f0ec7a6aa14e
comfy <comfy@comfy.li>
2017-01-18 13:12:19 +0100

    Documented classnames main.h a bit mroe

--- src/nogova_liberation.noe/meta/classnames/main.h
+++ src/nogova_liberation.noe/meta/classnames/main.h
@@ -1,29 +1,29 @@
 // Main Objects
-<#define MAIN_FOB_TYPE|"Land_Cargo_HQ_V1_F">
-<#define MAIN_FOB_BOX|"B_Slingload_01_Cargo_F">
-<#define MAIN_TRUCK|"B_Truck_01_box_F">
-<#define MAIN_TRUCK_RESPAWN|"B_Truck_01_medical_F">
-<#define MAIN_TRUCK_AMMOBOX_OPFOR|"O_Truck_03_transport_F">
-<#define MAIN_ARSENAL_BOX|"B_supplyCrate_F">
-<#define MAIN_TRANSPORT_HELO|"B_Heli_Transport_03_unarmed_F">
-<#define MAIN_AMMOBOX_BLUFOR|"Box_NATO_AmmoVeh_F">
-<#define MAIN_AMMOBOX_OPFOR|"Box_East_AmmoVeh_F">
+<#define MAIN_FOB_TYPE|"Land_Cargo_HQ_V1_F">                    // FOB Base Object
+<#define MAIN_FOB_BOX|"B_Slingload_01_Cargo_F">                 // FOB Packaged Box (For Slingloading)
+<#define MAIN_TRUCK|"B_Truck_01_box_F">                         // FOB Transport Truck
+<#define MAIN_TRUCK_RESPAWN|"B_Truck_01_medical_F">             // Mobile Respawn Vehicle
+<#define MAIN_TRUCK_AMMOBOX_OPFOR|"O_Truck_03_transport_F">     // Opfor transport truck Definition
+<#define MAIN_ARSENAL_BOX|"B_supplyCrate_F">                    // Arsenal Box Classname
+<#define MAIN_TRANSPORT_HELO|"B_Heli_Transport_03_unarmed_F">   // Main Transport helicopter (Transporting FOB Capable)
+<#define MAIN_AMMOBOX_BLUFOR|"Box_NATO_AmmoVeh_F">              // Blufor Ammbox Definition
+<#define MAIN_AMMOBOX_OPFOR|"Box_East_AmmoVeh_F">               // Opfor Ammobox Definition
 
 // Blufor Units
-<#define BLUFOR_UNIT_SQL|"B_Soldier_SL_F">
-<#define BLUFOR_UNIT_FTL|"B_Soldier_TL_F">
-<#define BLUFOR_UNIT_RIFLEMAN|"B_soldier_F">
-<#define BLUFOR_UNIT_GRENADIER|"B_soldier_GL_F">
-<#define BLUFOR_UNIT_AUTORIFLEMAN|"B_soldier_AR_F">
-<#define BLUFOR_UNIT_MEDIC|"B_medic_F">
-<#define BLUFOR_UNIT_MARKSMAN|"B_soldier_M_F">
-<#define BLUFOR_UNIT_ENGINEER|"B_engineer_F">
-<#define BLUFOR_UNIT_LAT|"B_soldier_LAT_F">
-<#define BLUFOR_UNIT_HAT|"B_soldier_AT_F">
-<#define BLUFOR_UNIT_AA|"B_soldier_AA_F">
-<#define BLUFOR_UNIT_MMG|"B_HeavyGunner_F">
-<#define BLUFOR_UNIT_SNIPER|"B_sniper_F">
-<#define BLUFOR_UNIT_CREWMAN|"B_crew_F">
-<#define BLUFOR_UNIT_PILOT|"B_Pilot_F">
-<#define BLUFOR_UNIT_HELIPILOT|"B_helipilot_F">
-<#define BLUFOR_UNIT_PARATROOPER|"B_soldier_PG_F">
+<#define BLUFOR_UNIT_SQL|"B_Soldier_SL_F">            // Squadleader
+<#define BLUFOR_UNIT_FTL|"B_Soldier_TL_F">            // Fireteam Leader
+<#define BLUFOR_UNIT_RIFLEMAN|"B_soldier_F">          // Rifleman
+<#define BLUFOR_UNIT_GRENADIER|"B_soldier_GL_F">      // Grenadier
+<#define BLUFOR_UNIT_AUTORIFLEMAN|"B_soldier_AR_F">   // Autorifleman (LMG)
+<#define BLUFOR_UNIT_MEDIC|"B_medic_F">               // Medic
+<#define BLUFOR_UNIT_MARKSMAN|"B_soldier_M_F">        // Marksman
+<#define BLUFOR_UNIT_ENGINEER|"B_engineer_F">         // Engineer
+<#define BLUFOR_UNIT_LAT|"B_soldier_LAT_F">           // Light AT
+<#define BLUFOR_UNIT_HAT|"B_soldier_AT_F">            // Dedicated AT
+<#define BLUFOR_UNIT_AA|"B_soldier_AA_F">             // Anti Air
+<#define BLUFOR_UNIT_MMG|"B_HeavyGunner_F">           // Medium Machine Gunner
+<#define BLUFOR_UNIT_SNIPER|"B_sniper_F">             // Sniper / Sharpshooter
+<#define BLUFOR_UNIT_CREWMAN|"B_crew_F">              // Crewman
+<#define BLUFOR_UNIT_PILOT|"B_Pilot_F">               // Jet Pilot
+<#define BLUFOR_UNIT_HELIPILOT|"B_helipilot_F">       // Helicopter Pilot
+<#define BLUFOR_UNIT_PARATROOPER|"B_soldier_PG_F">    // Paratrooper
