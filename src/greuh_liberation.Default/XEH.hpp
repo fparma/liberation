aef03963d125d43fa7f01735b02fc676d46200c5
Cuel <Cuel@users.noreply.github.com>
2017-02-06 13:23:04 +0100

    Add display unload EH

--- src/greuh_liberation.Default/XEH.hpp
+++ src/greuh_liberation.Default/XEH.hpp
@@ -3,3 +3,9 @@ class Extended_DisplayLoad_EventHandlers {
     FPARMA_ARSENAL_LIMITATION = "if (!isNil 'FPC_fnc_overrideArsenal') then {_this call FPC_fnc_overrideArsenal};";
   };
 };
+
+class Extended_DisplayUnload_EventHandlers {
+  class RscDisplayArsenal {
+    FPARMA_SAVE_GEAR = "profileNamespace setVariable [format ['%1_fpcGear', GRLIB_save_key], getUnitLoadout player]; saveProfileNamespace";
+  };
+};
